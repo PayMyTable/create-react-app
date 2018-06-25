8577c457c9a949788c5172ce38fb15e28649c7bb
Jean-Sébastien Eude <jse@paymytable.com>
2018-06-25 11:41:18 +0200

    restore hashed filename for css/js files and write assets file to build

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -22,6 +22,7 @@ const paths = require('./paths');
 const getClientEnvironment = require('./env');
 
 const autoImportConfig = require('./import/config')
+const FileSystem = require("fs");
 
 //const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
@@ -47,7 +48,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/styles.css';
+const cssFilename = 'static/css/[name].[contenthash:8].css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
@@ -76,7 +77,7 @@ module.exports = {
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
     // filename: 'static/js/[name].[chunkhash:8].js',
-    filename: 'static/js/bundle.js',
+    filename: 'static/js/[name].[chunkhash:8].js',
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath,
@@ -417,7 +418,21 @@ module.exports = {
     //paths: true,
     //chaining: true,
     //}),
-
+    function() {
+      this.plugin("done", function (statsData) {
+        var stats = statsData.toJson();
+        if (!stats.errors.length) {
+          var assets = {
+            js: stats.assetsByChunkName.main[0],
+            css: stats.assetsByChunkName.main[1]
+          }
+          FileSystem.writeFileSync(
+            path.join(__dirname, '../../../../build', 'assets.js'),
+            JSON.stringify(assets)
+          );
+        }
+      });
+    }
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
